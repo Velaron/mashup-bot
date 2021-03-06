bdc264eb158007fcf06cccd3c5fa703f255c92e6
RomkaZVO <romkaeliseev@gmail.com>
2021-03-06 16:47:30 +0800

    переход на Opus

--- DSAud.py
+++ DSAud.py
@@ -1,4 +1,4 @@
-from discord import FFmpegPCMAudio
+from discord import FFmpegOpusAudio
 from discord.ext.commands import Bot
 import discord
 import subprocess
@@ -18,7 +18,7 @@ client = Bot(command_prefix=list(config['prefix']))
 
 def parse_song_name_status(): #main status
     mpdclient = MPDClient()
-    mpdclient.connect('62.109.19.65', '6600')
+    mpdclient.connect(config['status_host'], config['status_port'])
     song_data = mpdclient.currentsong()
     if 'title' not in song_data:
         return os.path.splitext(os.path.basename(song_data['file']))[0]
@@ -31,7 +31,6 @@ def parse_song_name():
     mpdclient.connect(DATA['mpd_hostname'], int(DATA['mpd_port']))
     if DATA['mpd_password']:
 	    mpdclient.password(DATA['mpd_password'])
-
     song_data = mpdclient.currentsong()
     if 'title' not in song_data:
         return os.path.splitext(os.path.basename(song_data['file']))[0]
@@ -48,30 +47,36 @@ async def status_task():
 async def send_track(ctx):
     track = parse_song_name()
     track_message = discord.Embed(title='Текущий трек:', description=track, colour=discord.Color.green())
+    global msg
     msg = await ctx.send(embed=track_message)
     while True:
-        track_new = parse_song_name()
-        track_upd = discord.Embed(title='Текущий трек:', description=track_new, colour=discord.Color.green())
-        track_message = await msg.edit(embed=track_upd)
-        await asyncio.sleep(5)
+        track = parse_song_name()
+        track_upd = discord.Embed(title='Текущий трек:', description=track, colour=discord.Color.green())
+        await msg.edit(embed=track_upd)
+        await asyncio.sleep(8)
+
+async def delete(ctx):
+    global msg
+    delete = msg
+    await msg.delete()
 
 @client.event
 async def on_ready():
     client.loop.create_task(status_task())
-    print('Бот запущен!\nИмя: {}' + str(client.user.name))
+    print('DSAud is started!\nBot name: ' + str(client.user.name))
 
 @client.event
 async def on_guild_join(guild):
     for channel in guild.text_channels:
         if channel.permissions_for(guild.me).send_messages:
-            embed = discord.Embed(title='Спасибо, что вы добавили бота к себе на сервер!', description='Для ознакомления с командами напиши ``'+config['prefix']+'h``', colour=discord.Color.green())
-            await channel.send(embed=embed)
+            msg = discord.Embed(title='Спасибо, что вы добавили бота к себе на сервер!', description='Для ознакомления с командами напиши ``'+config['prefix']+'h``', colour=discord.Color.green())
+            await channel.send(embed=msg)
         break
 
 @client.command(aliases=['h'])
 async def hlp(ctx):
-    help_message = discord.Embed(title='Команды бота:', description='Основное:\n'+config['prefix']+'p- включить радио\n'+config['prefix']+'s- остановить\n'+config['prefix']+'c- сменить канал\n______________________\nРадиостанции:\n0) mashup radio - beats to napas\n1) mashup radio - 1.kla$ only\n2) Random rock radio [24/7] || RockCafe Radio\n\nПожалуйста, останавливайте бота, когдв выходите', colour=discord.Color.green())
-    await ctx.send(embed=help_message)
+    msg = discord.Embed(title='Команды бота:', description='Основное:\n'+config['prefix']+'p- включить радио\n'+config['prefix']+'s- остановить\n'+config['prefix']+'c- сменить канал\n______________________\nРадиостанции:\n0) mashup radio - beats to napas\n1) mashup radio - 1.kla$ only\n2) Random rock radio [24/7] || RockCafe Radio\n\nПожалуйста, останавливайте бота, когдв выходите', colour=discord.Color.green())
+    await ctx.send(embed=msg)
 
 @client.command(aliases=['p'])
 async def play(ctx, change=False):
@@ -79,8 +84,6 @@ async def play(ctx, change=False):
     if not change:
         main_message = discord.Embed(title='Привет! Подожди, идет кеширование\nНе забывай писать ``'+config['prefix']+'s`` для остановки, когда выходишь', colour=discord.Color.green())
         msg = await ctx.send(embed=main_message)
-        await asyncio.sleep(2)
-        await msg.delete()
     else: 
         main_message = discord.Embed(title='Подожди! Идет смена сервера!\nНе забывай писать ``'+config['prefix']+'s`` для остановки, когда выходишь', colour=discord.Color.green())
         msg = await ctx.send(embed=main_message)
@@ -94,7 +97,7 @@ async def play(ctx, change=False):
     except:
         pass
     FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 10', 'options': '-vn'}
-    player.play(FFmpegPCMAudio(DATA['source'],**FFMPEG_OPTIONS))
+    player.play(FFmpegOpusAudio(DATA['source'],**FFMPEG_OPTIONS))
     await send_track(ctx)
 
 @client.command(aliases=['s'])
@@ -103,6 +106,7 @@ async def stop(ctx):
     player.stop()
     await ctx.guild.voice_client.disconnect()
     await ctx.send('Пока!')
+    await delete(ctx)
     player = None
 
 @client.command(aliases=['c'])
@@ -110,12 +114,10 @@ async def change(ctx):
     splited = ctx.message.content.split()
     if(splited[1:]):
         index = int(splited[1]) if splited[1].isdigit() else None
-
         if index == None:
             msg = discord.Embed(title='Ошибка!', description='Значение должно быть числом', colour=discord.Color.green())
             await ctx.send(embed=msg)
             return
-        
         if index > 2:
             msg = discord.Embed(title='Ошибка!', description='Такого радио нет', colour=discord.Color.green())
             await ctx.send(embed=msg)
